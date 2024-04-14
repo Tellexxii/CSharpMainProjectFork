4163e450a6bbd6d5095e7feaebd5ab153cfeee96
GaGa <tellexxii@gmail.com>
2024-04-14 13:20:35 +0400

    [upd] logic to closest target

--- Assets/Scripts/UnitBrains/Player/SecondUnitBrain.cs
+++ Assets/Scripts/UnitBrains/Player/SecondUnitBrain.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Model.Runtime.Projectiles;
 using UnityEngine;
 
@@ -43,10 +44,24 @@ namespace UnitBrains.Player
             // Homework 1.4 (1st block, 4rd module)
             ///////////////////////////////////////
             List<Vector2Int> result = GetReachableTargets();
-            while (result.Count > 1)
+
+            float closestDistance = float.MaxValue;
+            Vector2Int closestTarget = result.First();
+
+            foreach (Vector2Int target in result)
             {
-                result.RemoveAt(result.Count - 1);
+                float dist = DistanceToOwnBase(target);
+                
+                if (dist < closestDistance)
+                {
+                    closestDistance = dist;
+                    closestTarget = target;
+                }
             }
+            
+            result.Clear();
+            result.Add(closestTarget);
+            
             return result;
             ///////////////////////////////////////
         }
